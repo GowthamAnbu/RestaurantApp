bab9cf34d89b81531f90f355c5d052462a9f188d
GOWTHAM <GOWTHAM@LENOVO>
2017-01-18 14:17:22 +0530

    check_input_length-FUNCTION added

---
+++ Restaurant/Functions/check_input_length.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+DROP FUNCTION IF EXISTS `check_input_length`$$
+CREATE FUNCTION `check_input_length`(litem_name TEXT,lquantity TEXT) 
+RETURNS BOOLEAN
+BEGIN
+DECLARE result BOOLEAN DEFAULT FALSE;
+DECLARE item_length INT DEFAULT 0;
+DECLARE quantity_length INT DEFAULT 0;
+/* storing number of items and number of quantities into local variables */
+SELECT LENGTH(litem_name)-LENGTH(REPLACE(litem_name,',',''))+1 INTO item_length;
+SELECT LENGTH(lquantity)-LENGTH(REPLACE(lquantity,',',''))+1 INTO quantity_length;
+IF (item_length<>quantity_length)THEN
+SET result=TRUE;
+END IF;
+RETURN result;
+END$$
+DELIMITER ;
