4b98530a8bcee0f3a313f24d4be840db7d54852e
GOWTHAM <GOWTHAM@LENOVO>
2017-01-18 13:08:26 +0530

    comments added to the valid seat number-FUNCTION

--- Restaurant/Functions/isseat_number.sql
+++ Restaurant/Functions/isseat_number.sql
@@ -4,6 +4,7 @@ CREATE FUNCTION `isseat_number`(lseat_number TINYINT)
 RETURNS BOOLEAN
 BEGIN 
 DECLARE result BOOLEAN DEFAULT FALSE;
+/* checking whether given seat number exists */
 IF (EXISTS(SELECT SEAT_NUMBER FROM SEED_SEAT WHERE SEAT_NUMBER=lseat_number)) THEN
 SET result=TRUE;
 END IF;
