cf6372ea271cd542912e79092de226f9451a7fb0
GOWTHAM <GOWTHAM@LENOVO>
2017-01-18 14:24:54 +0530

    comments added to check_input_length-FUNCTION

--- Restaurant/Functions/check_input_length.sql
+++ Restaurant/Functions/check_input_length.sql
@@ -9,6 +9,7 @@ DECLARE quantity_length INT DEFAULT 0;
 /* storing number of items and number of quantities into local variables */
 SELECT LENGTH(litem_name)-LENGTH(REPLACE(litem_name,',',''))+1 INTO item_length;
 SELECT LENGTH(lquantity)-LENGTH(REPLACE(lquantity,',',''))+1 INTO quantity_length;
+/* checking the number of items with the number of quantity */
 IF (item_length<>quantity_length)THEN
 SET result=TRUE;
 END IF;
